d003fea2cad8164fa946f1edf4a7c5398db08d1e
hugyy85 <antonbat125a@yandex.ru>
2020-05-21 10:01:03 +0700

    add format_file validation

--- kombain_service/kombain_service/forms.py
+++ kombain_service/kombain_service/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=128)
     file = forms.FileField()
+
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        formats_file = ['pdf', 'html']
+        split_file = file.name.lower().split('.')
+        format_file = split_file[len(split_file)-1]
+        if format_file not in formats_file:
+            raise ValidationError(f'Поддерживаются только {formats_file} форматы')
